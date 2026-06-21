afeeaa261a0756bf7567f664b0835fb441eb8abf
cuijunhao20000307-web <cuijunhao20000307@icloud.com>
2026-06-22 07:57:40 +0900

    feat(font): embed Noto Sans CJK SC in binary

---
+++ source/noto_sans_cjk_sc.s
@@ -0,0 +1,9 @@
+.section .rodata
+.balign 16
+
+.global nx_shell_chinese_font_otf
+nx_shell_chinese_font_otf:
+.incbin "../res/NotoSansCJKsc-Regular.otf"
+
+.global nx_shell_chinese_font_otf_end
+nx_shell_chinese_font_otf_end:
